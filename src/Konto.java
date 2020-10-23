293283221efa6b57484297856c9bff1203d2cf48
Ahmad Shereef <ad.sh1993@yahoo.dk>
2020-10-23 13:31:46 +0200

    første commit i Konto

--- src/Konto.java
+++ src/Konto.java
@@ -1,2 +1,12 @@
 public class Konto {
+    int balance;
+
+    public Konto() {
+        this.balance = 1000;
+    }
+    addPoint(
+
+    public int getBalance() {
+        return balance;
+    }
 }
