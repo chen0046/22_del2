5ac12f41fc9b90752ce0509fb59e170074baf7a6
Andreas <s205450@student.dtu.dk>
2020-10-23 12:04:49 +0200

    Lavet ændringer i spiller

--- src/Spiller.java
+++ src/Spiller.java
@@ -2,32 +2,30 @@ import java.util.Scanner;
 
 public class Spiller {
 
-        int point; //Point skal defineres som Int da det involverer hele tal
-        String navn; //Navn skal defineres som String da det involverer tekst
+    String navn; //Navn skal defineres som String da det involverer tekst
 
-        public String getNavn() {
-            return navn;
-        } //Her gør vi så når man skriver getNavn så får man det definerede Navn
+    public String getNavn() {
+        return navn;
+    } //Her gør vi så når man skriver getNavn så får man det definerede Navn
 
-        public void setNavn(String navn) {
-            this.navn = navn;
-        } //Her gør vi så når man skriver setNavn så definerer vi navnet
+    public void setNavn(String navn) {
+        this.navn = navn;
+    } //Her gør vi så når man skriver setNavn så definerer vi navnet
 
-        public int getPoint() {
+       /* public int getPoint() {
             return point;
         } //Når man skriver getPoint så viser den ens point
 
         public void setPoint(int point) {
             this.point = point;
-        } //Når man skriver setPoint så opdaterer den værdien af ens point
+        } //Når man skriver setPoint så opdaterer den værdien af ens point */
+
+
+        Scanner input = new Scanner(System.in);
+
+
+        public void askName (Spiller currentPlayer){
+            currentPlayer.setNavn(input.nextLine());
+        }
     }
-
-public class AskName {
-    Scanner input = new Scanner(System.in);
-
-
-    public void askName(Spiller currentPlayer){
-        currentPlayer.setNavn(input.nextLine());
-    }
-}
 
