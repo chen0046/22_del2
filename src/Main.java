56f5bf3d786c0594aa126559159f6dbade62f6d1
isabeljacobsen <isabe@grimmig.dk>
2020-10-28 12:44:45 +0100

    Spilleregler:

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ public class Main {
     static Spiller spiller1 = new Spiller();
     static Spiller spiller2 = new Spiller();
     static Tur tur = new Tur();
+    static Printregler regler = new Printregler();
 
     public static void main(String[] args) {
 
+        regler.Spilleregler();
         System.out.println("Skriv navnet på spiller1");
         spiller1.askName(spiller1);
         System.out.println("Spiller 1 er " + spiller1.getNavn());
         System.out.println("Skriv navnet på spiller2");
         spiller2.askName(spiller2);
         System.out.println("Spiller 2 er " + spiller2.getNavn());
+
 
         tur.tur(spiller1);
 
--- src/Printregler.java
+++ src/Printregler.java
@@ -1,2 +1,21 @@
 public class Printregler {
+    public void Spilleregler() {
+        System.out.println("Spilleregler");
+        System.out.println();
+        System.out.println("Spillet indeholder:");
+        System.out.println("2 teringer");
+        System.out.println("2 spillere");
+        System.out.println("11 felter, mellem 2 og 12");
+        System.out.println();
+
+        System.out.println("Regler:");
+        System.out.println();
+        System.out.println("1) Spiller 1 og 2 skiftes til at slå med to terninger");
+        System.out.println("2) Begge spillere starter med 1000 kroner");
+        System.out.println("3) Antalet af terningeøjne afgøre hvilket felt spilleren lander på");
+        System.out.println("4) Hvet felt indeholder en værdig (positiv eller negativ), som bliver tilføjet til spilleres pengebeholdning");
+        System.out.println("5) Spillerne kan ikke skylde penge, d.v.s pengebeholdningen kan ikke være under 0");
+        System.out.println("6) Spilleren der først opnår beløbet 3000 kroner, vinder ");
+
+    }
 }
